995f0b22dd20585e169feabc241d75af27fb5119
sujeethkumar16 <sujeethkumar1622@gmail.com>
2026-05-19 17:07:27 +0530

    Add files via upload

---
+++ May/Code.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+import ast
+import pickle
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+movies = pd.read_csv("tmdb_5000_movies.csv")
+credits = pd.read_csv("tmdb_5000_credits.csv")
+
+movies = movies.merge(credits, on="title")
+
+movies = movies[['movie_id','title','overview','genres','keywords','cast','crew']]
+
+def convert(text):
+    L = []
+    for i in ast.literal_eval(text):
+        L.append(i['name'])
+    return L
+
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
+
+def convert_cast(text):
+    L = []
+    counter = 0
+    for i in ast.literal_eval(text):
+        if counter != 3:
+            L.append(i['name'])
+            counter += 1
+        else:
+            break
+    return L
+
+movies['cast'] = movies['cast'].apply(convert_cast)
+
+def fetch_director(text):
+    L = []
+    for i in ast.literal_eval(text):
+        if i['job'] == 'Director':
+            L.append(i['name'])
+    return L
+
+movies['crew'] = movies['crew'].apply(fetch_director)
+
+movies.dropna(inplace=True)
+
+movies['overview'] = movies['overview'].apply(lambda x: x.split())
+
+def collapse(L):
+    return [i.replace(" ", "") for i in L]
+
+movies['genres'] = movies['genres'].apply(collapse)
+movies['keywords'] = movies['keywords'].apply(collapse)
+movies['cast'] = movies['cast'].apply(collapse)
+movies['crew'] = movies['crew'].apply(collapse)
+movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+
+new_df = movies[['movie_id','title','tags']]
+
+new_df['tags'] = new_df['tags'].apply(lambda x: " ".join(x))
+
+new_df['tags'] = new_df['tags'].apply(lambda x: x.lower())
+
+tfidf = TfidfVectorizer(max_features=5000, stop_words='english')
+
+vectors = tfidf.fit_transform(new_df['tags']).toarray()
+
+similarity = cosine_similarity(vectors)
+
+def recommend(movie):
+    movie_index = new_df[new_df['title'] == movie].index[0]
+    distances = similarity[movie_index]
+
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    for i in movies_list:
+        print(new_df.iloc[i[0]].title)
+
+recommend("Avatar")
+
+pickle.dump(new_df, open('movies.pkl','wb'))
+pickle.dump(similarity, open('similarity.pkl','wb'))
+pickle.dump(tfidf, open('vectorizer.pkl','wb'))
